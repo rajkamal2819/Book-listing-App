76bfbd78d270cbc37f85216bfaada0685346907e
Rajkamal <rajkamal.singh1011@gmail.com>
2021-10-11 19:15:08 +0530

    Commit: Created Log in Activity and 2 fragments for log in and signUp.

---
+++ app/src/main/java/com/learn/booklistapp/Authentication.java
@@ -0,0 +1,29 @@
+package com.learn.booklistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.learn.Adapters.FragmentAdapter;
+
+public class Authentication extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+    }
+}
